667521464078d5dfac14ac4b2bf31456f759ebf0
Lucas Lannes <lucas.lannes123@gmail.com>
2024-02-27 07:03:22 -0300

    uploading to github

--- resources/js/Components/Event/Event.tsx
+++ resources/js/Components/Event/Event.tsx
@@ -4,9 +4,9 @@ export default function Event(
         return (
             <>
                 
-                <div className="grid border p-4 rounded shadow-md">
+                <div className="flex flex-wrap gap-4 border p-4 rounded shadow-md bg-clr-30 text-counter-clr-30 cursor-pointer">
                     <div>
-                        <img src="" alt="" />
+                        <img src="https://as2.ftcdn.net/v2/jpg/00/91/03/83/1000_F_91038333_C6b2EeBuxMcVjJpvdtdG2dMZijoSUkDX.jpg" alt="event-picture" className="w-64 rounded" />
                     </div>
                     <div>
                         <h2>{eventName}</h2>
